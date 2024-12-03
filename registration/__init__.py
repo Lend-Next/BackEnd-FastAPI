31ca857896fa4696ff10b2600d87cf9d80fb7e1c
Sougata Mandal <sougata.mandal@cloudkaptan.com>
2024-12-03 15:42:24 +0530

    ref: deleted files

--- registration/__init__.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-# Cognito User Pool Settings
-USER_POOL_ID = 'ap-south-1_CMRp1sN4W'
-CLIENT_ID = 'hkibiomfmpuh9m0oorc1rknkk'
-REGION = 'ap-south-1'
-JWT_SECRET= 'your-jwt-secret-key'
-
-cognito_client = boto3.client('cognito-idp',region_name=REGION)
