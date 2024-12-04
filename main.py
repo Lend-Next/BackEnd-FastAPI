b3bf00adef39689189379c54782b62a966b0f9bf
Anshu <anshu.gupta@cloudkaptan.com>
2024-12-04 17:12:01 +0530

    feat: added application

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from persons.models import Base  # Import the Base from your models
 from cognito_user_info.routes import router as user_info_router
 from fastapi.middleware.cors import CORSMiddleware
 from registration.routes import router as registration_router
+from application.routes import router as application_router
 
 
 app = FastAPI(title="CK-LendNext API")
@@ -25,6 +26,7 @@ app.add_middleware(
 app.include_router(registration_router, prefix="/registration", tags=["Registration"])
 app.include_router(persons_router, prefix="/persons", tags=["Persons"])
 app.include_router(user_info_router, prefix="/current-user", tags=["User Info"])
+app.include_router(application_router, prefix="/application", tags=["Application"])
 
 
 @app.get("/")
